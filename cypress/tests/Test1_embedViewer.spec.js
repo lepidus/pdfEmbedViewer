9106060cdd59d5cb6e46b592354b14a8740beba9
Jhon <jhon@lepidus.com.br>
2025-06-16 14:04:31 -0400

    Adds integration tests
    Issue: documentacao-e-tarefas/scielo#785
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/Test1_embedViewer.spec.js
@@ -0,0 +1,13 @@
+
+describe('Embed PDF viewer', function () {
+	it('Checks presence of PDF embed viewer at workflows', function () {
+		cy.login('dbarnes', null, 'publicknowledge');
+        cy.findSubmission('archive', 'Arguments About Arguments');
+
+        cy.get('#publication-button').click();
+        cy.get('#galleys-button').click();
+
+        cy.contains('a', 'PDF');
+        cy.get('#pdfEmbedViewer').should('exist');
+	});
+});
